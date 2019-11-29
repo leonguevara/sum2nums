2c39be56748d11f1cfbbc27018698e78623f96e1
León Guevara <leon.guevara@tec.mx>
2019-11-29 11:05:20 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cout << "Give me two integer numbers: ";
+    cin >> x >> y;
+    cout << x << " + " << y << " = " << (x + y) << endl;
+    return 0;
+}
